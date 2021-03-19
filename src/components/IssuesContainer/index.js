5c550d413cf8980ef7a3eeab05896f079f609b0d
Prashant Pokhriyal <prashant.pokhriyal@vedantu.com>
2021-03-19 20:55:43 +0530

    show repo name and owner

--- src/components/IssuesContainer/index.js
+++ src/components/IssuesContainer/index.js
@@ -3,6 +3,7 @@ import { Tabs, Tab, Button } from 'react-bootstrap';
 import IssuesTable from '../IssuesTable';
 
 export default function IssuesContainer(props) {
+    const [repo, setRepo] = useState({});
     const [issues, setIssues] = useState({});
     const [page, setPage] = useState(1);
     const [loading, setLoading] = useState(true);
@@ -15,8 +16,14 @@ export default function IssuesContainer(props) {
     const handleSearch = () => toggleSearchOpenIssues(!searchOpenIssues);
 
     useEffect(() => {
+        fetch('https://api.github.com/repos/angular/angular')
+            .then(res => res.json())
+            .then(res => setRepo(res));
+    });
+
+    useEffect(() => {
         setLoading(loading => true);
-        fetch(`https://api.github.com/search/issues?q=repo:angular/angular/node+type:issue${searchOpenIssues ? '+state:open' : ''}&p%20er_page=${perPage}&page=${page}`)
+        fetch(`https://api.github.com/search/issues?q=repo:angular/angular/node+type:issue${searchOpenIssues ? '+state:open' : ''}&per_page=${perPage}&page=${page}`)
             .then(res => res.json())
             .then(res => {
                 setIssues(res);
@@ -25,23 +32,26 @@ export default function IssuesContainer(props) {
     }, [page, searchOpenIssues, perPage]);
 
     return (
-        <Tabs defaultActiveKey="issues" id="uncontrolled-tab-example">
-            <Tab eventKey="issues" title="Issues">
-                <Button variant="primary" onClick={handleSearch}>Search: Open Issues</Button>{' '}
-                {
-                    loading
-                        ?
-                        'Loading...'
-                        :
-                        <IssuesTable
-                            issues={issues}
-                            currentPage={page}
-                            onPageChange={handlePageChange}
-                            skipPages={skipPages}
-                            startPageIndex={startPageIndex}
-                            perPage={perPage} />
-                }
-            </Tab>
-        </Tabs>
+        <>
+            <h3>{repo.owner?.login} / {repo.name}</h3>
+            <Tabs defaultActiveKey="issues" id="uncontrolled-tab-example">
+                <Tab eventKey="issues" title="Issues">
+                    <Button variant="primary" onClick={handleSearch}>Search: Open Issues</Button>{' '}
+                    {
+                        loading
+                            ?
+                            'Loading...'
+                            :
+                            <IssuesTable
+                                issues={issues}
+                                currentPage={page}
+                                onPageChange={handlePageChange}
+                                skipPages={skipPages}
+                                startPageIndex={startPageIndex}
+                                perPage={perPage} />
+                    }
+                </Tab>
+            </Tabs>
+        </>
     );
 }
